b2c466ac82f4b09d0a099c4e45d3ada8a906753d
Yaasirakin <yasirakinfenwa@gmail.com>
2025-07-24 22:55:19 -0700

    new

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { ThemeProvider } from './component/ThemeContext.js'
+import { ThemeProvider } from './components/ThemeContext.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
           <ThemeProvider>
